fdacfe75a93fcc74daa14fa0a2039d0abe5d3047
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-10-19 14:39:27 +0300

    [Feature] Add Viewsets for CRUD operations on resources

--- my_blog/blog_api/views.py
+++ my_blog/blog_api/views.py
@@ -1,6 +1,26 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from rest_framework import viewsets
 
-from django.shortcuts import render
+from .models import Category, Article
+from .serializers import CategorySerializer, ArticleSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    Allow for CRUD functionality for a category resource
+    Request methods (api/v1/categories): POST, GET
+    Request methods (api/v1/categories/<id>): GET, PUT, DELETE
+    """
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    Allow for CRUD functionality for a article resource
+    Request methods (api/v1/articles): POST, GET
+    Request methods (api/v1/articles/<id>): GET, PUT, DELETE
+    """
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
