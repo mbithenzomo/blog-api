dce7b1ecf84b5222c3d156b9ea6765bd722928f1
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-10-19 15:28:55 +0300

    [Feature] Add login view

--- my_blog/blog_api/views.py
+++ my_blog/blog_api/views.py
@@ -1,7 +1,31 @@
-from rest_framework import viewsets
+from django.contrib.auth import authenticate
+
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Category, Article
-from .serializers import CategorySerializer, ArticleSerializer
+from .serializers import CategorySerializer, ArticleSerializer, UserSerializer
+
+
+class LoginView(APIView):
+    """
+    Allow admin user to login
+    """
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            user = authenticate(username=request.data["username"],
+                                password=request.data["password"])
+            if user is not None:
+                return Response(serializer.data,
+                                status=status.HTTP_200_CREATED)
+            else:
+                return Response({"error": "Invalid username or password."},
+                                status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
