298ddd941e8a1b025eb969d45c981a3b6725e1e2
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-10-19 15:29:09 +0300

    [Feature] Add login view

--- my_blog/blog_api/urls.py
+++ my_blog/blog_api/urls.py
@@ -1,9 +1,12 @@
+from django.conf.urls import url
+
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, ArticleViewSet
+from .views import CategoryViewSet, ArticleViewSet, LoginView
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'articles', ArticleViewSet)
 
 urlpatterns = router.urls
+urlpatterns.append(url(r'^auth/login$', LoginView.as_view()))
