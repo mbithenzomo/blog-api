b235c320c49289e5215d964f16e3889621c90040
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-10-19 14:22:59 +0300

    Add Category and Blog model

--- my_blog/blog_api/models.py
+++ my_blog/blog_api/models.py
@@ -2,5 +2,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Defines a blog category
+    """
+
+    name = models.CharField(max_length=150)
+
+    def __unicode__(self):
+        return "Category: {}".format(self.name)
+
+
+class Article(models.Model):
+    """
+    Defines a blog article
+    """
+
+    writer = models.ForeignKey(User, related_name="articles")
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    category = models.ForeignKey(Category, related_name="articles")
+    image = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return "Article: {}".format(self.title)
