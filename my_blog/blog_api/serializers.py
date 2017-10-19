d13ded171b74cebedcc647333c15c3545c1e8bf4
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-10-19 14:25:56 +0300

    [Feature] Add model serializers

---
+++ my_blog/blog_api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Article, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        fields = '__all__'
