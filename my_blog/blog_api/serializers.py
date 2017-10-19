18e4b10ee6bf6846cedcc7f0be91ace86895a5fd
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-10-19 15:37:01 +0300

    [Feature] Add user serializer for login

--- my_blog/blog_api/serializers.py
+++ my_blog/blog_api/serializers.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
 from .models import Article, Category
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializeqr):
 
     class Meta:
         model = Category
@@ -15,3 +17,21 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(
+        max_length=None,
+        min_length=None,
+        allow_blank=False)
+
+    password = serializers.CharField(
+        max_length=100,
+        style={'input_type': 'password'},
+        required=True,
+        write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
